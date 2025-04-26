afeaa25172f7218d57c31e9c21bdf809bf85e6dc
saifalafeefi <saif.alafeefi2003@gmail.com>
2025-04-26 21:44:08 +0400

    Remove target directory from tracking and update gitignore
    Co-Authored-By: Zayed <126242098+al9eri@users.noreply.github.com>

--- c4_rust/target/debug/deps/c4_rust-33ed76f18629dc44.d
+++ c4_rust/target/debug/deps/c4_rust-33ed76f18629dc44.d
@@ -1,8 +1,8 @@
-C:\Users\Smiley\OneDrive - ku.ac.ae\University\2025-1 Spring\Principles of Programming Languages\Project\c4_rust\target\debug\deps\libc4_rust-33ed76f18629dc44.rmeta: src\lib.rs src\lexer.rs src\parser.rs src\vm.rs
+C:\Users\Smiley\Documents\GitHub\c4_rust_jumeirah\c4_rust\target\debug\deps\libc4_rust-33ed76f18629dc44.rmeta: src\lib.rs src\lexer.rs src\parser.rs src\vm.rs
 
-C:\Users\Smiley\OneDrive - ku.ac.ae\University\2025-1 Spring\Principles of Programming Languages\Project\c4_rust\target\debug\deps\libc4_rust-33ed76f18629dc44.rlib: src\lib.rs src\lexer.rs src\parser.rs src\vm.rs
+C:\Users\Smiley\Documents\GitHub\c4_rust_jumeirah\c4_rust\target\debug\deps\libc4_rust-33ed76f18629dc44.rlib: src\lib.rs src\lexer.rs src\parser.rs src\vm.rs
 
-C:\Users\Smiley\OneDrive - ku.ac.ae\University\2025-1 Spring\Principles of Programming Languages\Project\c4_rust\target\debug\deps\c4_rust-33ed76f18629dc44.d: src\lib.rs src\lexer.rs src\parser.rs src\vm.rs
+C:\Users\Smiley\Documents\GitHub\c4_rust_jumeirah\c4_rust\target\debug\deps\c4_rust-33ed76f18629dc44.d: src\lib.rs src\lexer.rs src\parser.rs src\vm.rs
 
 src\lib.rs:
 src\lexer.rs:
--- c4_rust/target/debug/deps/c4_rust.d
+++ c4_rust/target/debug/deps/c4_rust.d
@@ -1,6 +1,6 @@
-C:\Users\Smiley\OneDrive - ku.ac.ae\University\2025-1 Spring\Principles of Programming Languages\Project\c4_rust\target\debug\deps\c4_rust.exe: src\main.rs src\lexer.rs src\parser.rs src\vm.rs
+C:\Users\Smiley\Documents\GitHub\c4_rust_jumeirah\c4_rust\target\debug\deps\c4_rust.exe: src\main.rs src\lexer.rs src\parser.rs src\vm.rs
 
-C:\Users\Smiley\OneDrive - ku.ac.ae\University\2025-1 Spring\Principles of Programming Languages\Project\c4_rust\target\debug\deps\c4_rust.d: src\main.rs src\lexer.rs src\parser.rs src\vm.rs
+C:\Users\Smiley\Documents\GitHub\c4_rust_jumeirah\c4_rust\target\debug\deps\c4_rust.d: src\main.rs src\lexer.rs src\parser.rs src\vm.rs
 
 src\main.rs:
 src\lexer.rs:
